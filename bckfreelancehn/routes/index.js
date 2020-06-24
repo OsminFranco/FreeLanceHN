6bfc2893a96ebac9b330390850fe731ac7974584
Osmin franco <tonyfranco_28@icloud.com>
2020-06-24 16:29:29 -0600

    primeras rutas

--- bckfreelancehn/routes/index.js
+++ bckfreelancehn/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res)=> {
+  res.status(403).json({"msg":"No implementado"})
 });
+
+
+router.get('/sec/login', async (req, res)=> {
+  res.status(403).json({"msg":"No implementado"})
+});
+
 
 module.exports = router;
