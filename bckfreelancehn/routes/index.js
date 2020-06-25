15a55c41db281c034c499f9b9858fe53e1440d8b
Silvia Sevilla <sindelsevilla10@gmail.com>
2020-06-25 17:22:44 -0600

    Rutas Empleo/one/:id y Empleo/aplicar/:id

--- bckfreelancehn/routes/index.js
+++ bckfreelancehn/routes/index.js
@@ -19,6 +19,14 @@ router.get('/Empleo/all', async (req, res)=> {
   res.status(403).json({"msg":"No implementado"})
 });
 
+router.get('/Empleo/one/:id', async (req, res)=> {
+  res.status(403).json({"msg":"No implementado"})
+});
+
+router.put('/Empleo/aplicar/:id', async (req, res)=> {
+  res.status(403).json({"msg":"No implementado"})
+});
+
 router.post('/Empleo/new', async (req, res)=> {
   res.status(403).json({"msg":"No implementado"})
 });
